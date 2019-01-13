d29942388809c5570fdf7f06759ca488a5e64f0f
Francisco Izurieta <franizus@live.com>
2019-01-13 11:27:52 -0500

    padding and aligment in page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,12 +8,22 @@ class ProductPage extends StatelessWidget {
         title: Text('Product Detail'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text('Details!'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
-          )
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details!'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
         ],
       ),
     );
